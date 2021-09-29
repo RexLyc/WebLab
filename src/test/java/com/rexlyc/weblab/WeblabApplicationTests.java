be702b081381b4341d8bcf9c282a02dd91e8199c
RexLyc <911570983@qq.com>
2021-09-30 04:23:58 +0800

    my web lab init

---
+++ src/test/java/com/rexlyc/weblab/WeblabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rexlyc.weblab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
